1816a5c540cba2a239f73944096692309251f4f2
keandre <keandre.imposter@gmail.com>
2025-07-08 10:43:24 +0700

    fixed json loading and creating

--- taskcli.py
+++ taskcli.py
@@ -1,17 +1,16 @@
 import sys
 import json
 arguments = sys.argv
-data_template = """
-{
+data_template = {
     tasks: 
     {
         
     }
-}"""
+}
 try:
     with open("data.json","r") as file:
         data = json.load(file)
 except:
     with open("data.json","w") as file:
         print("Json file does not exist, creating json template...")
-        json.dump(data_template,file)
+        json.dump(data_template,file,indent=2)
