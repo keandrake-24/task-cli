9d4a7e15641f1a32d822ff7c5c40cdb01b6fbf69
keandre <keandre.imposter@gmail.com>
2025-07-08 10:39:05 +0700

    renamed main.py into taskcli.py and added basic json data loading and creating alongside error handling

---
+++ taskcli.py
@@ -0,0 +1,17 @@
+import sys
+import json
+arguments = sys.argv
+data_template = """
+{
+    tasks: 
+    {
+        
+    }
+}"""
+try:
+    with open("data.json","r") as file:
+        data = json.load(file)
+except:
+    with open("data.json","w") as file:
+        print("Json file does not exist, creating json template...")
+        json.dump(data_template,file)
